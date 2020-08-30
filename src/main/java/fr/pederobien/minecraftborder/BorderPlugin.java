f1eada5d994cf82cac5270ff972a1bd929cc747e
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-08-30 11:40:30 +0200

    Adding file plugin.yml and class BorderPlugin

---
+++ src/main/java/fr/pederobien/minecraftborder/BorderPlugin.java
@@ -0,0 +1,48 @@
+package fr.pederobien.minecraftborder;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.pederobien.dictionary.interfaces.IDictionaryParser;
+import fr.pederobien.minecraftgameplateform.utils.Plateform;
+
+public class BorderPlugin extends JavaPlugin {
+	public static final String NAME = "minecraft-border";
+
+	@Override
+	public void onEnable() {
+		Plateform.getPluginHelper().register(this);
+		new BorderCommand(this);
+		registerDictionaries();
+	}
+
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+
+	private void registerDictionaries() {
+		// Registering French dictionaries
+		registerDictionary("French", "Border.xml");
+
+		// Registering English dictionaries
+		registerDictionary("English", "Border.xml");
+	}
+
+	private void registerDictionary(String parent, String... dictionaryNames) {
+		Path jarPath = Plateform.ROOT.getParent().resolve(NAME.concat(".jar"));
+		String dictionariesFolder = "resources/dictionaries/".concat(parent).concat("/");
+		for (String name : dictionaryNames)
+			registerDictionary(Plateform.getDefaultDictionaryParser(dictionariesFolder.concat(name)), jarPath);
+	}
+
+	private void registerDictionary(IDictionaryParser parser, Path jarPath) {
+		try {
+			Plateform.getNotificationCenter().getDictionaryContext().register(parser, jarPath);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
