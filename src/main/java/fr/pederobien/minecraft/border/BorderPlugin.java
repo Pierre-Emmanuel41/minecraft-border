c34800bb6740dab6517980932c0a717dc5cf74a5
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-01-02 00:26:41 +0100

    persistence: Updating signature of methods serialize and deserialize

--- src/main/java/fr/pederobien/minecraft/border/BorderPlugin.java
+++ src/main/java/fr/pederobien/minecraft/border/BorderPlugin.java
@@ -1,12 +1,9 @@
 package fr.pederobien.minecraft.border;
 
-import java.io.FileNotFoundException;
-
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.pederobien.dictionary.exceptions.MessageRegisteredException;
 import fr.pederobien.dictionary.impl.JarXmlDictionaryParser;
 import fr.pederobien.minecraft.border.commands.border.BorderCommandTree;
 import fr.pederobien.minecraft.border.interfaces.IBorder;
@@ -56,22 +53,18 @@ public class BorderPlugin extends JavaPlugin {
 	}
 
 	private void registerDictionaries() {
-		try {
-			JarXmlDictionaryParser dictionaryParser = new JarXmlDictionaryParser(getFile().toPath());
+		JarXmlDictionaryParser dictionaryParser = new JarXmlDictionaryParser(getFile().toPath());
 
-			MinecraftDictionaryContext context = MinecraftDictionaryContext.instance();
-			String[] dictionaries = new String[] { "English.xml", "French.xml" };
-			for (String dictionary : dictionaries)
-				try {
-					context.register(dictionaryParser.parse(DICTIONARY_FOLDER.concat(dictionary)));
-				} catch (MessageRegisteredException e) {
-					AsyncConsole.print(e);
-					for (StackTraceElement element : e.getStackTrace())
-						AsyncConsole.print(element);
-				}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		MinecraftDictionaryContext context = MinecraftDictionaryContext.instance();
+		String[] dictionaries = new String[] { "English.xml", "French.xml" };
+		for (String dictionary : dictionaries)
+			try {
+				context.register(dictionaryParser.parse(DICTIONARY_FOLDER.concat(dictionary)));
+			} catch (Exception e) {
+				AsyncConsole.print(e);
+				for (StackTraceElement element : e.getStackTrace())
+					AsyncConsole.print(element);
+			}
 	}
 
 	private void registerTabExecutors() {
